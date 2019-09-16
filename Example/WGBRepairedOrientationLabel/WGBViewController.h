22bf97d3e84910676ad114e66b32e9993551f0dc
YGC <guchengye153@gmail.com>
2019-09-16 21:35:46 +0700

    Initial commit

---
+++ Example/WGBRepairedOrientationLabel/WGBViewController.h
@@ -0,0 +1,13 @@
+//
+//  WGBViewController.h
+//  WGBRepairedOrientationLabel
+//
+//  Created by Wangguibin on 09/16/2019.
+//  Copyright (c) 2019 Wangguibin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WGBViewController : UIViewController
+
+@end
---
+++ Example/WGBRepairedOrientationLabel/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WGBRepairedOrientationLabel
+//
+//  Created by Wangguibin on 09/16/2019.
+//  Copyright (c) 2019 Wangguibin. All rights reserved.
+//
+
+@import UIKit;
+#import "WGBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WGBAppDelegate class]));
+    }
+}
